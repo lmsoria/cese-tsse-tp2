c643f83eade0ba9c0a911672a2fa0aa55698929e
Leandro Soria <leandrosoria@hotmail.com>
2024-03-16 12:20:15 -0300

    Documentada API en header

--- inc/leds.h
+++ inc/leds.h
@@ -27,7 +27,6 @@ SPDX-License-Identifier: MIT
 
 /* === Headers files inclusions ================================================================ */
 
-#include <stdbool.h>
 #include <stdint.h>
 
 /* === Cabecera C++ ============================================================================ */
@@ -41,13 +40,39 @@ extern "C" {
 /* === Public variable declarations ============================================================ */
 /* === Public function declarations ============================================================ */
 
+/// @brief Inicializa el puerto de LEDs, con todos ellos apagados.
+/// @param port puerto a inicializar.
+/// @return 0 en caso de éxito.
 int leds_init(uint16_t * port);
+
+/// @brief Deinicializa el puerto de LEDs previamente inicializado, dejándolo NULL
 void leds_deinit(void);
+
+/// @brief Encender el LED deseado
+/// @param led LED a encender.
+/// @return 0 en caso de éxito, o -1 si el puerto no se encuentra inicializado.
 int leds_turn_on_single(uint16_t led);
+
+/// @brief Apagar el LED deseado
+/// @param led LED a apagar.
+/// @return 0 en caso de éxito, o -1 si el puerto no se encuentra inicializado.
 int leds_turn_off_single(uint16_t led);
+
+/// @brief Consultar el estado del LED deseado
+/// @param led LED a consultar.
+/// @return 1 si el LED está prendido, 0 si está apagado o -1 si el puerto no está inicializado.
 int leds_get_status_single(uint16_t led);
+
+/// @brief Prender todos los LEDs del puerto.
+/// @return 0 en caso de éxito, o -1 si el puerto no se encuentra inicializado.
 int leds_turn_on_all(void);
+
+/// @brief Apagar todos los LEDs del puerto.
+/// @return 0 en caso de éxito, o -1 si el puerto no se encuentra inicializado.
 int leds_turn_off_all(void);
+
+/// @brief Consultar el estado de toda la máscara de LEDs del puerto.
+/// @return Máscara del puerto, o -1 si el puerto no se encuentra inicializado.
 int leds_get_status_all(void);
 
 /* === End of documentation ==================================================================== */
